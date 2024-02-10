68e8d5782d026c07733f064e3251c2edb802a533
IsisChameleon <12711084+IsisChameleon@users.noreply.github.com>
2024-02-10 04:07:15 +0000

    Refactored scraper to extract search results and added example for disabling headless mode for debugging.

--- src/scraper/scraper.go
+++ src/scraper/scraper.go
@@ -8,6 +8,9 @@ import (
 	"time"
 
 	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/input"
+	"github.com/go-rod/rod/lib/launcher"
+	"github.com/go-rod/rod/lib/utils"
 )
 
 // Article represents the structure of our JSON output
@@ -19,21 +22,14 @@ type Article struct {
 	ArticleLink string   `json:"article_link"`
 }
 
-func scrape() {
-	// Launch a new browser with default options, and connect to it.
-	browser := rod.New().MustConnect()
-	defer browser.MustClose()
-
-	// Navigate to the target webpage
-	page := browser.MustPage("https://www.frontiersin.org/search?query=bacteriophage&tab=articles&origin=https%3A%2F%2Fwww.frontiersin.org%2Fjournals").MustWaitLoad()
-	log.Println("Browser started")
-
+func extract_search_results(page *rod.Page) []Article {
 	// Extract the required data
 	var articles []Article
-	elements := page.MustElements("#article-results > ul > li")
-	for _, el := range elements {
+	elements := page.MustElements("#article-results > ul")
+	for i, el := range elements {
 		title := el.MustElement("div.data-top > div.title").MustText()
-		authorsEl := el.MustElements("div.data-top > ul > li")
+		authorEl_Selector := fmt.Sprintf("#article-results > ul > li:nth-child(%d) > a > div.data-top > ul", i)
+		authorsEl := page.MustElements(authorEl_Selector)
 		var authors []string
 		for _, authorEl := range authorsEl {
 			authors = append(authors, authorEl.MustText())
@@ -50,6 +46,24 @@ func scrape() {
 			ArticleLink: link,
 		})
 	}
+	return articles
+}
+
+func scrape() {
+	// Launch a new browser with default options, and connect to it.
+	browser := rod.New().MustConnect()
+	// Even you forget to close, rod will close it after main process ends.
+	defer browser.MustClose()
+
+	// Navigate to the target webpage
+	// page := browser.MustPage("https://www.frontiersin.org/search?query=bacteriophage&tab=articles&origin=https%3A%2F%2Fwww.frontiersin.org%2Fjournals").MustWaitLoad()
+	// log.Println("Browser started")
+
+	page := browser.MustPage("https://www.frontiersin.org/search?tab=articles")
+
+	page.MustElement("#search_query_input").MustInput("bacteriophage").MustType(input.Enter)
+
+	articles := extract_search_results(page)
 
 	// Convert articles to JSON
 	jsonArticles, err := json.Marshal(articles)
@@ -76,6 +90,47 @@ func scrape() {
 	}
 
 	log.Printf("Data written to file: %s\n", filename)
+}
+
+// Shows how to disable headless mode and debug.
+// Rod provides a lot of debug options, you can set them with setter methods or use environment variables.
+// Doc for environment variables: https://pkg.go.dev/github.com/go-rod/rod/lib/defaults
+func Example_disable_headless_to_debug() {
+	// Headless runs the browser on foreground, you can also use flag "-rod=show"
+	// Devtools opens the tab in each new tab opened automatically
+	l := launcher.New().
+		Headless(false).
+		Devtools(true)
+
+	defer l.Cleanup()
+
+	url := l.MustLaunch()
+
+	// Trace shows verbose debug information for each action executed
+	// SlowMotion is a debug related function that waits 2 seconds between
+	// each action, making it easier to inspect what your code is doing.
+	browser := rod.New().
+		ControlURL(url).
+		Trace(true).
+		SlowMotion(2 * time.Second).
+		MustConnect()
+
+	// ServeMonitor plays screenshots of each tab. This feature is extremely
+	// useful when debugging with headless mode.
+	// You can also enable it with flag "-rod=monitor"
+	launcher.Open(browser.ServeMonitor(""))
+
+	defer browser.MustClose()
+
+	page := browser.MustPage("https://www.frontiersin.org/search?tab=articles")
+
+	page.MustElement("#search_query_input").MustInput("bacteriophage").MustType(input.Enter)
+
+	articles := extract_search_results(page)
+
+	fmt.Println(articles)
+
+	utils.Pause() // pause goroutine
 }
 
 func main() {
